b41a500b5e72027a75429cd8ad5af38f4a074fa3
pedro <pedro9@github.com>
2016-03-13 16:29:29 -0300

    initial commit

---
+++ automate_1.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+# test save
+
+
+# In[ ]:
+
+import numpy as np
+
+
+# In[ ]:
+
+print np.arange(10)
+
+
+# In[ ]:
+
+get_ipython().system(u'pwd')
+
+
+# In[ ]:
+
+get_ipython().system(u'ls')
+
+
+# In[ ]:
+
+get_ipython().system(u'cat automate_1.py')
+
+
+# In[ ]:
+
+
+
